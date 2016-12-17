b1bdf7aa99ba38e2fd2d50739b9bbd6c5e0f6146
szymongor <gornioczek.szymon@gmail.com>
2016-12-17 14:50:51 +0100

    Schedule Converter

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
+var apiClient = new ApiClient('http://localhost');
+var scheduleConverter = new ScheduleConverter([],5,13);
+
+//apiClient.getGroupSchedule("GroupA",scheduleConverter.convertJSONToArray); <-- example
+
 class scheduleManager {
   constructor(schedule) {
     this.schedule = schedule;
   }
-  
+
   getScheduleById(scheduleId) {
 	  document.getElementById(scheduleId).innerHTML = scheduleId;
   }
@@ -11,9 +16,9 @@ class scheduleManager {
 var showSchedule = function(id) {
 	var scheduleId = id + "Schedule";
 	var schedule = document.getElementById(scheduleId);
-	
+
 	var sc = new scheduleManager("plan zajec");
 	sc.getScheduleById(scheduleId);
-	
+
 	schedule.classList.toggle("hidden");
 }
