38f403228e2c19a083a8fb242dbab43a9573de64
szymongor <gornioczek.szymon@gmail.com>
2016-12-17 13:56:07 +0100

    Get group API, error handling
    New XML Schedule

--- Server/Schedule.php
+++ Server/Schedule.php
@@ -3,35 +3,38 @@
 	$method = $_SERVER['REQUEST_METHOD'];
 		switch($method){
 			case 'GET':
-        $response = json_encode(getSchedule("GroupA"));
+      //http://localhost/Schedule/Server/Schedule.php?group=GroupA
+        $groupName = $_GET['group'];
+        $response = json_encode(getSchedule($groupName));
 				echo($response);
 				break;
 
     }
 
 function getSchedule($scheduleName){
-  $xmlFile = simplexml_load_file($scheduleName.".xml");
+  if($xmlFile = @simplexml_load_file($scheduleName.".xml")){
+    $scheduleArray = array();
 
-  $scheduleArray = array();
+    foreach ($xmlFile as $day) {
+      $dayArray = [];
+      $dayName = $day->attributes();
 
-  foreach ($xmlFile as $day) {
-    $dayArray = [];
-    $dayName = $day->attributes();
+      foreach ($day as $lesson) {
+        $lessonTime = $lesson->attributes()."";
+        $lessonName = $lesson."";
+        $dayArray[$lessonTime] = $lessonName;
+      }
 
-    echo "</br>";
+      $scheduleArray[$dayName.""] = $dayArray;
 
-    foreach ($day as $lesson) {
-      $lessonTime = $lesson->attributes()."";
-      $lessonName = $lesson."";
-      $dayArray[$lessonTime] = $lessonName;
+
     }
 
-    $scheduleArray[$dayName.""] = $dayArray;
-
-
+    return $scheduleArray;
+  }else{
+    return "File not present!";
   }
 
-  return $scheduleArray;
 }
 
 ?>
